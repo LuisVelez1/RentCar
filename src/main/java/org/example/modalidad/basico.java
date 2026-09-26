1d50deda808d72dd12d753ccc82bdfc495779c25
Johan Gomez <johans.gomezg@uqvirtual.edu.co>
2026-09-26 03:00:54 -0500

    creamos la clase basico del package modalidades

---
+++ src/main/java/org/example/modalidad/basico.java
@@ -0,0 +1,11 @@
+package org.example.modalidad;
+public class basico extends Modalidad{
+    public basico(String codigo, double valorDiario) {
+        super(codigo, "Basico", "Vehículo básico", 1, valorDiario, "Kilometraje limitado");
+    }
+
+    @Override
+    public double calcularCostoTotal(int dias) {
+        return dias * valorDiario;
+    }
+}
