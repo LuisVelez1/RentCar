524abe80c6ff93f665725a635b7373d389fe5282
Johan Gomez <johans.gomezg@uqvirtual.edu.co>
2026-09-26 03:04:03 -0500

    creamos la clase modalidadFactory

---
+++ src/main/java/org/example/modalidad/modalidadFactory.java
@@ -0,0 +1,15 @@
+package org.example.modalidad;
+
+public class modalidadFactory {
+
+    public static Modalidad crearModalidad(String tipo, String codigo, double valorDiario) {
+        if (tipo.equalsIgnoreCase("Basico")) {
+            return new basico(codigo, valorDiario);
+        } else if (tipo.equalsIgnoreCase("Ejecutiva")) {
+            return new Ejecutiva(codigo, valorDiario);
+        } else if (tipo.equalsIgnoreCase("Premium")) {
+            return new Premium(codigo, valorDiario, "Cobertura Total", 2, "Asientos de excelente calidad y GPS");
+        }
+        throw new IllegalArgumentException("Tipo de modalidad no válida");
+    }
+}
