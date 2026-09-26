7dbf9816671409b2b9db9c7567b53abd37f0df61
Johan Gomez <johans.gomezg@uqvirtual.edu.co>
2026-09-26 02:58:05 -0500

    creamos la clase vehiculo

---
+++ src/main/java/org/example/vehiculo.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class vehiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private Integer ano;
+    private String tipo;
+    private Double tarifaDiaria;
+
+    public vehiculo (String placa, String marca, String modelo, Integer ano, String tipo, Double tarifaDiaria){
+        this.placa=placa;
+        this.marca=marca;
+        this.modelo=modelo;
+        this.ano=ano;
+        this.tipo=tipo;
+        this.tarifaDiaria=tarifaDiaria;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Double getTarifaDiaria() {
+        return tarifaDiaria;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setTarifaDiaria(Double tarifaDiaria) {
+        this.tarifaDiaria = tarifaDiaria;
+    }
+}
